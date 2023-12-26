d59d724ba534d6ac2791b983bb11083d43064a71
wise_JG <59231743+Hju95@users.noreply.github.com>
2023-12-26 17:26:54 +0900

    추상클래스 연습문제 Q5,Q6

---
+++ src/ch9q/Car.java
@@ -0,0 +1,22 @@
+package ch9q;
+
+public abstract class Car {
+
+    public abstract void start();
+    public abstract void drive();
+    public abstract void stop();
+    public abstract void turnOff();
+
+
+    public void run() {
+        start();
+        drive();
+        stop();
+        turnOff();
+        washCar();
+    }
+
+    public void washCar() {
+        System.out.println("세차를 합니다");
+    }
+}
